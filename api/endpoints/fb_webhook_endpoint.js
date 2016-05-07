bad1a42dd390582a318f68cbfc6d9dc9941d4a01
Tomer Coreanu <tomercor@gmail.com>
2016-05-07 18:40:40 -0400

    logging

--- api/endpoints/fb_webhook_endpoint.js
+++ api/endpoints/fb_webhook_endpoint.js
@@ -12,19 +12,21 @@ var challengeToken =  function (req, res) {
 
 var handleIncomingMessage =  function (req, res) {
   var messagingEvents = req.body.entry[0].messaging;
-  console.log(req.body);
+  console.log(JSON.stringify(req.body));
   messagingEvents.forEach(event => {
     console.log(event);
     var sender = event.sender.id;
     var postback = event.postback;
 
+    /*
     var attachments = event.message.attachments;
     if (attachments != undefined && attachments.length > 0) {
       attachments.forEach(attachment => {
         console.log(attachment);
       });
     }
-
+    */
+    
     var text = event.message && event.message.text;
     FbMessage.receive.handleFbMessage(sender, postback, text);
   });
