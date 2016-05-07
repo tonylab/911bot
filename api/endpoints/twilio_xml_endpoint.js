69a24e632e2852d8bf118cb9011afe0f83505803
Hadar Landao <hlandao@gmail.com>
2016-05-07 18:45:42 -0400

    added content to twilio xml endpoint

--- api/endpoints/twilio_xml_endpoint.js
+++ api/endpoints/twilio_xml_endpoint.js
@@ -1,16 +1,67 @@
 const xml = require('xml');
+const cases = require('../../modules/cases_global')
 
 const callStep1 = function (req, res) {
   const caseId = req.query.caseId;
   if(!caseId) {
-    let error = new Error('Case id is missing');
+    var error = new Error('Case id is missing');
     error.httpCode = 400;
     throw  error;
+  } else if (!cases[caseId]) {
+      var error = new Error('Case not found');
+      error.httpCode = 400;
+      throw  error;
   }
-  const xml = `<?xml version="1.0" encoding="UTF-8" ?><Response><Say>Hello World</Say></Response>`;
+
+  const caseObj = cases[caseId];
+  const actionUrl = `twilio/call/step2`
+  const xmlResponse = {
+      response : [
+          {
+              say: `This is an emergency call from ${caseObj.name}`
+          },
+          {
+              pause: {_attr: {length: 2}}
+          },
+          {
+              say: caseObj.message
+          },
+          {
+              pause: {_attr: {length: 2}}
+          },
+          {
+              gather: [
+                  {
+                      _attr: {
+                          action: actionUrl,
+                          method: `GET`,
+                          timeout: 10,
+                          numDigits : 1,
+                          finishOnKey: 'any digit'
+                      }
+                  },
+                  {
+                      say: `Dial 1 in order to connect with the user`
+                  }
+              ]
+          },
+          {
+              say: `Please call ${caseObj.phoneNumber}`
+          },
+          {
+              pause: {_attr: {length: 5}}
+          },
+          {
+              say: `Please call ${caseObj.phoneNumber}`
+          },
+          {
+              say: `Thank you and goodbye!`
+          }
+      ]
+  };
 
   res.set('Content-Type', 'text/xml')
-  res.send(xml);
+  res.send(xml(xmlResponse));
 }
 
 const callStep2 = function () {
