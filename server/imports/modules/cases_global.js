beb2afc3ce89e118e6ca34eb56a84d7d99a14630
Hadar Landao <hlandao@gmail.com>
2016-05-07 23:54:52 -0400

    update twilio endpoints with new case structure

--- server/imports/modules/cases_global.js
+++ server/imports/modules/cases_global.js
@@ -1,7 +1,12 @@
 export default {
     test: {
+        _id: "test",
         name: "Tomer",
         phoneNumber: "+972545800086",
-        message: "I was kidnapped. help me. help!!"
+        message: "I was kidnapped. help me. help!!",
+        location: {
+            lat: 48.8580,
+            lng: 2.294
+        }
     }
 };
