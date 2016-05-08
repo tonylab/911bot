2c7107ac1b5e8f1cbe46236358059ef702b4086f
Tomer Coreanu <tomercor@gmail.com>
2016-05-08 01:31:40 -0400

    Added inner cases for car accident

--- server/imports/modules/steps/data.js
+++ server/imports/modules/steps/data.js
@@ -13,7 +13,80 @@ export default {
             image: 'http://res.cloudinary.com/hqkcoueyo/image/upload/v1462675405/car_accident_suyuj6.jpg',
             buttons: [{
               payloadKey: '11TrafficAccident',
-              title: 'Report'
+              title: 'Report',
+              innerStep: {
+                type: 'bubbles',
+                bubbles:[
+                  {
+                    title: 'Check yourself first',
+                    subtitle: 'If you have been injured in the accident, first check yourself for any injuries. Try to assess how well you can move your limbs, and if you experience symptoms such as dizziness etc. Remember you need to be fit enough to help the others',
+                    image: '',
+                    buttons: [{
+                      payloadKey: 'TAI1',
+                      title: 'I am ok',
+                      innerStep: {
+                        type: 'bubbles',
+                        bubbles:[
+                          {
+                            title: 'Do not move any injured person',
+                            subtitle: ' Do not move any injured person unless there is a fire or a strong smell of fuel that is caused by a leak. moving him/her in such a situation can cause more harm than good.',
+                            image: '',
+                            buttons: [{
+                              payloadKey: 'TAI2',
+                              title: 'Got it',
+                              innerStep: {
+                                type: 'bubbles',
+                                bubbles:[
+                                  {
+                                    title: 'Secure the Accident Scene',
+                                    subtitle: 'Park your car with hazard lights and/or headlights on ideally facing approaching traffic. Place a warning triangle in the road.If there are other people who can help send them back along the road to wave traffic in order to slow it down. Take care on fast moving roads. Other drivers might not understand what you are trying to do.',
+                                    image: '',
+                                    buttons: [{
+                                      payloadKey: 'TAI3',
+                                      title: 'Done',
+                                      innerStep: {
+                                        type: 'bubbles',
+                                        bubbles:[
+                                          {
+                                            title: 'Reduce risks',
+                                            subtitle: 'Check the scene, switch off engines, impose a no smoking ban. Keep children at a safe distance.',
+                                            image: '',
+                                            buttons: [{
+                                              payloadKey: 'TAI4',
+                                              title: 'Done',
+                                              innerStep: {
+                                                type: 'bubbles',
+                                                bubbles:[
+                                                  {
+                                                    title: 'Send us a short video',
+                                                    subtitle: 'Sending us a short video will help us asses the damage and send the most accurate help',
+                                                    image: '',
+                                                    buttons: [{
+                                                      payloadKey: 'TAI5',
+                                                      title: 'Done',
+                                                      innerStep: {
+
+                                                      }
+                                                    }]
+                                                  }
+                                                ]
+                                              }
+                                            }]
+                                          }
+                                        ]
+                                      }
+                                    }]
+                                  }
+                                ]
+                              }
+                            }]
+                          }
+                        ]
+                      }
+                    }]
+                  }
+                ]
+              }
             }]
           },
           {
