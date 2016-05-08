30c92ff37009b83f45c8aaaffca0e3fcc60bd4b6
okland <omri.klinger@gmail.com>
2016-05-07 20:36:33 -0400

    Basic steps son

--- README.md
+++ README.md
@@ -6,3 +6,17 @@
 3. npm start
 
 
+
+
+######### Basic cases
+
+
+# 1. Suspicious
+# 2. Traffic reporting - This category differs from traffic accidents because it involves reports of drunk or reckless drivers, as well as road hazards, such as fallen trees.
+# 3. Traffic Accidents
+# 4. Disturbances. - These calls include minor disturbances, such as shouting, as well as physical altercations, including anything that involves a weapon, Myers said.
+# 5. Alarms.  This category includes residential and commercial burglary and robbery alarms. It does not include fire alarms.
+# 6. Welfare checks.
+# 7. Civil problems
+# 8. Thefts.
+# 9. Sick person.
---
+++ modules/steps/index.js
@@ -0,0 +1,82 @@
+var baseSteps = {
+  type: 'button',
+  text: 'Please choose:',
+  options: [
+    {
+      "text": "Life threatening",
+      innerStep: {
+        type: 'bubbles',
+        bubbles: [
+          {
+            text: 'Traffic Accident',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Violence',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Health Emergency',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Fire',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Other',
+            image: '',
+            button: ''
+          }
+        ]
+      }
+    },
+    {
+      "text": "Not life threatening",
+      innerStep: {
+        type: 'bubbles',
+        bubbles: [
+          {
+            text: 'Traffic reporting',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Disturbances',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Alarms',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Welfare checks',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Civil problems',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Health issues',
+            image: '',
+            button: ''
+          },
+          {
+            text: 'Other',
+            image: '',
+            button: ''
+          }
+        ]
+      }
+    }
+  ]
+};
