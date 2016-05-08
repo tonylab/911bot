574de866e29462976e64dd14bdff62e378424794
Tomer Coreanu <tomercor@gmail.com>
2016-05-08 05:49:02 -0400

    added images and bubble

--- server/imports/modules/steps/data.js
+++ server/imports/modules/steps/data.js
@@ -15,8 +15,65 @@ export default {
               payloadKey: 'A Traffic Accident',
               title: 'Report',
               innerStep: {
-                type: 'image',
-                image: 'http://res.cloudinary.com/hqkcoueyo/image/upload/v1462699634/carSteps_shfyqm.jpg'
+                type: 'imageAndBubble',
+                image: 'http://res.cloudinary.com/hqkcoueyo/image/upload/v1462699634/carSteps_shfyqm.jpg',
+                bubbles: [
+                  {
+                    title: '1.Check the other person(s) for injuries',
+                    subtitle: 'If other people are injured, first assess the extent of his/ her injuries.',
+                    buttons: [{
+                      payloadKey: 'HOW TO ASSES',
+                      title: 'HOW TO ASSES'
+                    }]
+                  },
+                  {
+                    title: '2.Look for signs of breathing',
+                    subtitle: 'check if the person is breathing and if he has a pulse.'
+                  },
+                  {
+                    title: '3.Check for obstructions in mouth and throat',
+                    subtitle: 'If you do not hear any breath sounds, check his/her mouth for any obstructions. If there is something obstructing the airway, use your index and middle finger to clear the airway.'
+                  },
+                  {
+                    title: '4.Perform life saving techniques',
+                    subtitle: 'If there is no pulse, start CPR or EAR. Keep the person’s neck straight to start EAR or CPR',
+                    buttons: [
+                      {
+                        payloadKey: 'makeCPR',
+                        title: 'How to make CPR',
+                        innerStep: {
+                          type: 'image',
+                          image: 'http://res.cloudinary.com/hqkcoueyo/image/upload/v1462696950/cpr_j17pcn.jpeg'
+                        }
+                      },
+                      {
+                        payloadKey: 'makeEar',
+                        title: 'How to make EAR',
+                        innerStep: {
+                          type: 'image',
+                          image: 'http://res.cloudinary.com/hqkcoueyo/image/upload/v1462696950/ear_roesx6.jpg'
+                        }
+                      }
+                    ]
+                  },
+                  {
+                    title: '5.Ways to help in grave situations',
+                    subtitle: 'If there is bleeding from the mouth or the patient is vomiting, turn the person to his/her side. This will avoid any chances of the person choking. Place the person’s arm that is under him straight out and the arm closest to you across his chest.'
+                  },
+                  {
+                    title: '6.Deal with open wounds',
+                    subtitle: 'If there are extensive wounds, try to control the bleeding using pressure to the area using a cloth. Press down with your palms rather than your finger tips.'
+                  }
+                  ,
+                  {
+                    title: '7.Keep the person warm',
+                    subtitle: 'Usually accident victims feel excessively cold due to shock. Therefore keeping them warm is essential to survival. You can use whatever you have to do this, such as a T-shirt, jacket, etc.'
+                  },
+                  {
+                    title: '8.Avoid feeding the person',
+                    subtitle: 'Do not give the person any water, food or other fluids through the mouth, it could lead to the patient choking.'
+                  }
+                ]
               }
             }]
           },
